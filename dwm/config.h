f73a848f340ba27baf266e9ce6e256850a55a1cf
He Chunhui <hchunhui@mail.ustc.edu.cn>
2015-06-29 02:53:12 +0000

    new rule

--- dwm/config.h
+++ dwm/config.h
@@ -29,6 +29,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
 	{ "MPlayer",  NULL,       NULL,       0,            True,        -1 },
+	{ "mplayer2", NULL,       NULL,       0,            True,        -1 },
 	{ "qemu",     NULL,       NULL,       0,            True,        -1 },
 	{ "Thunar",   NULL,       "File Operation ", 0,     True,        -1 },
 	{ "Wicd-client", NULL,    NULL,       0,            True,        -1 },
